6b05543a055ba2a1fc80e430c454fcb70498c143
vaibhavpatil07615 <106390812+vaibhavpatil07615@users.noreply.github.com>
2023-05-04 10:17:06 +0530

    crud project using generic collection

---
+++ Assignemnt22apr.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myprojectadvance.Collection.Assignment
+{
+    internal class Assignemnt22apr
+    {
+     
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter string ");
+            string s = Console.ReadLine();
+            string op = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                op = op + s[i];
+
+            }
+            Console.WriteLine("reverse string = " + op);
+
+        }
+
+
+    }
+    class Armstrong
+    {
+        public static void checknum(int n)
+        {
+            int sum = 0;
+            int temp = n;
+            while (n > 0)
+            {
+                int rem = n % 10;
+                 sum = sum + rem * rem * rem;
+                n = n / 10;
+            }
+            if (temp == sum)
+            {
+                Console.WriteLine("number is armstrong");
+            }
+            else Console.WriteLine("number is not armstrong");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number to check armstrong or not");
+            int num = int.Parse(Console.ReadLine());
+            Armstrong.checknum(num);
+        }
+    }
+    class Minnumber
+    {
+        public static void Min(int[] a)
+        {
+            int min = a[0]; 
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] < min)
+                {
+                    min = a[i];
+                }
+            }
+            Console.WriteLine($"minimum element from array is = {min}");
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            Console.WriteLine($"enter {arr.Length} element of array");
+
+            for (int i = 0;i< arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Minnumber.Min(arr);
+        }
+    }
+   /* class Primenumber
+    {
+        static bool isprime (int n)
+        {
+            if (n== 0 || n == 1){
+
+                return false;
+            }
+            for (int i = 2;i< n; i++)
+            {
+                if (n%i == 0)
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int N = 100;
+            for (int i = 1; i<= N; i++)
+            {
+                if (isprime(i))
+                {
+                    Console.WriteLine(i + " ");
+                }
+            }
+
+
+
+
+
+
+
+
+
+        }*/
+    
+
+      /*  class Fibonacci
+        {
+            static void Main(string[] args)
+            {
+                int a = 1;
+                int b = 1;
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                int c;
+                for (int i = 1;i<= 5; i++)
+                {
+                    c = a + b;
+                    Console.WriteLine(c);
+                    a = b;
+                    b = c;
+
+                }
+
+            }
+        }*/
+    
+
+}
---
+++ Crudproject.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myprojectadvance.Collection.Assignment
+{
+
+    public class Student
+    {
+        public int rollno { get; set; }
+        public string name { get; set; }
+        public double percent { get; set; }
+
+    }
+    public class StudentCrud
+    {
+        private List<Student> students;
+        public StudentCrud()
+        {
+            students = new List<Student>();
+        }
+        public void Addstudent(Student stud)
+        {
+            students.Add(stud);
+        }
+        public void UpdateStudent(Student stud)
+        {
+            foreach (Student s in students)
+            {
+                if (s.rollno == stud.rollno)
+                {
+                    s.name = stud.name;
+                    s.percent = stud.percent;
+                }
+
+            }
+        }
+        public void Deletestudent(int Rollno)
+        {
+            foreach (Student s in students)
+            {
+                if (s.rollno == Rollno)
+                {
+                    students.Remove(s);
+                }
+            }
+        }
+        public List<Student> GetStudents()
+        {
+            return students;
+        }
+        public Student GetstudentById(int Rollno)
+        {
+            Student op = new Student();
+            foreach (Student p in students)
+            {
+                if (p.rollno == Rollno)
+                {
+                    op = p;
+                    break;
+                }
+            }
+            return op;
+        }
+    }
+    public class Maindata
+    {
+        static void Main(string[] args)
+        {
+            StudentCrud db = new StudentCrud();
+            char op;
+            do
+            {
+                Console.WriteLine("1. Add Student");
+                Console.WriteLine("2. Update Student");
+                Console.WriteLine("3. Delete Student");
+                Console.WriteLine("4. View All");
+                Console.WriteLine("5. View by roll no ");
+                Console.WriteLine("6. student with percentage less than 60");
+                int ch = Convert.ToInt32(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1:
+                        Student s = new Student();
+                        Console.WriteLine("enter roll no");
+                        s.rollno = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("enter name of student");
+                        s.name = Console.ReadLine();
+                        Console.WriteLine("enter percentage");
+                        s.percent = Convert.ToInt64(Console.ReadLine());
+                        db.Addstudent(s);
+                        break;
+                    case 2:
+                        Student v = new Student();
+                        Console.WriteLine("enter student roll no");
+                        v.rollno = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("enter student name");
+                        v.name = Console.ReadLine();
+                        Console.WriteLine("enter student percentage");
+                        v.percent = Convert.ToInt64(Console.ReadLine());
+                        db.UpdateStudent(v);
+                        break;
+                    case 3:
+                        Student t = new Student();
+                        Console.WriteLine("enter student roll no");
+                        int Rollno = Convert.ToInt32(Console.ReadLine());
+                        db.Deletestudent(Rollno);
+                        break;
+                    case 4:
+                        List<Student> list = db.GetStudents();
+                        Console.WriteLine(" roll no.\tname  \t percentage");
+                        foreach (Student c in list)
+                        {
+                            Console.WriteLine($"{c.rollno} \t {c.name} \t  {c.percent}");
+                        }
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter roll no");
+                        int rollno2 = Convert.ToInt32(Console.ReadLine());
+                        Student item2 = db.GetstudentById(rollno2);
+                        Console.WriteLine("Roll no  \t name \t percentage");
+                        Console.WriteLine($"{item2.rollno} \t {item2.name} \t {item2.percent}");
+                        break;
+                    default:
+                        Console.WriteLine("No option available");
+                        break;
+                }
+                Console.WriteLine("Do you want to continue ?? ");
+                Console.WriteLine("yes = y  no = N");
+
+                op = Convert.ToChar(Console.ReadLine());
+            }
+            while (op == 'y');
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+    
+
+
+
+
+
